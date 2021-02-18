fd416d40ff05d0e2d089cb2710719151e41b5644
SeelpAydin <aydinjabary98@gmail.com>
2021-02-18 09:34:22 +0330

    root url

--- fiverrClone/urls.py
+++ fiverrClone/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', index)
+
 ]
