04555964073466e33a6c5c3fca789cd9d53a3d60
Arshia Mohammadi <arshiamoh123@gmail.com>
2021-02-19 14:48:28 +0330

    update UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django_countries.fields import CountryField
 
 
 class UserProfile(AbstractUser):
-    pass
+    short_bio = models.CharField("write short bio", max_length=35)
+    description = models.TextField("long description")
+    date_joined = models.DateTimeField(auto_now_add=True)
+    country = CountryField()
+
 
     def __str__(self) -> str:
         return self.username
