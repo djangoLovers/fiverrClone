0cf2311db0c55f794d87d36496e363b455c3bd2e
SeelpAydin <aydinjabary98@gmail.com>
2021-02-23 01:08:05 +0330

    adding comment crud

--- gigs/urls.py
+++ gigs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import callback, index, show, new, edit, order
+from .views import index, show, new, edit, comment, order
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path('<int:id>/', show, name='show'),
     path('new/', new, name='new'),
     path('<int:id>/edit/', edit, name='edit'),
+    path('<int:id>/comment/', comment, name='comment'),
     path('<int:id>/order/', order, name='order')
 ]
