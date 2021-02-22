4bbeecb2ca656d517eaf7d7242afe38639a057a2
Arshia Mohammadi <arshiamoh123@gmail.com>
2021-02-23 01:19:56 +0330

    added orders/purchases urls pattern

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, show, edit
+from .views import index, show, edit, my_orders, my_purchases
 
 urlpatterns = [
     path('', index, name='index'),
     path('<int:id>/', show, name='show'),
-    path('<int:id>/edit/', edit, name='edit')
+    path('<int:id>/edit/', edit, name='edit'),
+    path('orders/', my_orders, name="orders"),
+    path('purchases/', my_purchases, name="purchases")
 ]
