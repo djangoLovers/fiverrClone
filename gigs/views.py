86042a3bdc3c1b73a4130cae9674ebc585d79ab3
Arshia Mohammadi <arshiamoh123@gmail.com>
2021-02-22 23:33:44 +0330

    chckoutView CBV

--- gigs/views.py
+++ gigs/views.py
@@ -1,20 +1,9 @@
-from typing import Match
 from django.contrib import messages
+from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from .forms import GigForm
 from .models import Category, Gig
-
-# Payment Imports
-
-import random
-from os import environ
-from django.urls import reverse, reverse_lazy
-from django.views.generic import View
-from django.http import HttpResponseBadRequest, HttpResponseForbidden
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from zeep import Client
 
 
 def index(request):
@@ -76,89 +65,6 @@ def edit(request, id):
         messages.error(request, "You Don't have The Permission to do that")
         return redirect('core:index')
 
-# Payment Shits
-
-
-class PaymentView(View):
-    template_name = 'gigs/payment.html'
-    messages = {
-        "invalid_amount": {
-            "level": messages.ERROR,
-            "text": "Invalid Amount"
-        },
-        "get_token_fail": {
-            "level": messages.ERROR,
-            "text": "Get Token Failed : %s"
-        },
-    }
-
-    def post(self, request, id):
-
-        gig = get_object_or_404(Gig, id=id)
-        amount = round(gig.price)
-        order_id = random.random()
-
-        if amount:
-            client = Client('https://api.nextpay.org/gateway/token.wsdl')
-            result = client.service.TokenGenerator(
-                environ.get('PAYMENT_API_KEY'),
-                order_id,
-                amount,
-                request.build_absolute_uri(reverse('gigs:payment_callback')),
-            )
-
-            if result.code != -1:
-                messages.add_message(
-                    self.request,
-                    self.messages["get_token_fail"]["level"],
-                    self.messages["get_token_fail"]["text"] % result.code
-                )
-            else:
-                return redirect('https://api.nextpay.org/gateway/payment/%s' % result.trans_id)
-
-        else:
-            messages.add_message(
-                self.request,
-                self.messages["invalid_amount"]["level"],
-                self.messages["invalid_amount"]["text"]
-            )
-
-        return redirect(self.get_fail_url())
-
-    def get_fail_url(self):
-        return str(reverse_lazy('gigs:payment'))
-
-    def get_success_url(self):
-        return str(reverse_lazy('gigs:payment'))
-
-
-class PaymentCallbackView(View):
-    template_name = 'gigs/callback.html'
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        return super(PaymentCallbackView, self).dispatch(request, *args, **kwargs)
-
-    def post(self, request):
-
-        trans_id = request.POST.get('trans_id')
-        order_id = request.POST.get('order_id')
-
-        amount = 2000
-
-        if trans_id and order_id and amount:
-            client = Client('https://api.nextpay.org/gateway/verify.wsdl')
-            result = client.service.PaymentVerification(
-                environ.get('PAYMENT_API_KEY'),
-                order_id,
-                amount,
-                trans_id,
-            )
-
-            return render(request, self.template_name, {
-                'result_code': result,
-                'trans_id': trans_id
-            })
-
-        else:
-            return HttpResponseBadRequest()
+#order view
+class CheckoutView(View):
+   pass 
