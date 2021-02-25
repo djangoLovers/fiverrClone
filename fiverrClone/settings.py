f598e0f4f8f8d68a51732799789c524792a7d8ce
SeelpAydin <aydinjabary98@gmail.com>
2021-02-25 23:45:57 +0330

    a

--- fiverrClone/settings.py
+++ fiverrClone/settings.py
@@ -126,6 +126,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = BASE_DIR / 'statics'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/statics/'
 STATICFILES_DIRS = [BASE_DIR / 'statics']
