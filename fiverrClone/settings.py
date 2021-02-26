78dbc22fd34df3d9c7263235f0ebe8d9d4ab016c
SeelpAydin <aydinjabary98@gmail.com>
2021-02-26 11:29:38 +0330

    postgre

--- fiverrClone/settings.py
+++ fiverrClone/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'fiverr-clone.herokuapp.com']
 
